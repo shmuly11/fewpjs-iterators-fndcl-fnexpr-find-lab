cb1e7baf736ac6c7e13d539b3050381523148370
shmuly11 <shmuly11@gmail.com>
2021-02-08 16:21:16 -0500

    commit tree 1e5bde2a84b0dbd7bd39ebd75bb74ec7168f37a5

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let winningYear = array.find(o => o.result === 'W')
+  return (winningYear ? winningYear.year : undefined)
+}
